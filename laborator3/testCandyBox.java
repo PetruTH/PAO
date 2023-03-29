49bcb3868d5d63a485e0e8670939cf14d1abb8bf
PetruTH <95080404+PetruTH@users.noreply.github.com>
2023-03-29 20:10:32 +0300

    Delete laborator3 directory

--- laborator3/testCandyBox.java
+++
@@ -1,8 +0,0 @@
-package laborator3;
-import laborator3.CandyBox;
-public class testCandyBox {
-    public static void main(String[] str){
-        CandyBox cb1 = new CandyBox("Bomboane", "Petru");
-        System.out.println(cb1.toString());
-    }
-}
