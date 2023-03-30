e3e64a8a59a0c3206fddd2c9a0de5ae72721bbd3
PetruTH <95080404+PetruTH@users.noreply.github.com>
2023-03-30 21:41:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//adauga exceptii pe colo colo nr stocuri e cat size produse nu mai poti adauga alte produse
-
         MainService service = MainService.getInstance();
         service.meniuDealerShip();
 
